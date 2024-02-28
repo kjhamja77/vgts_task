e37d5c1996c294054bc817184a9c286dc956d456
halith <techie3.ayani@gmail.com>
2024-02-28 10:08:56 +0530

    removed print and commented line

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,7 +19,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const BottomTab(),
-      //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
